0de44d54d8c213afc6f3747856b2732e047a8b01
shak-maker <mgshagai@gmail.com>
2025-07-25 15:49:25 +0800

    added api

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, Put, Delete } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 
@@ -9,5 +9,25 @@ export class ProductController {
   @Post()
   async create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.productService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.productService.findOne(+id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateProductDto: any) {
+    return this.productService.update(+id, updateProductDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return this.productService.remove(+id);
   }
 } 
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -13,4 +13,26 @@ export class UserService {
       data: { ...data, password: hashedPassword },
     });
   }
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async update(id: number, data: any) {
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, 10);
+    }
+    return this.prisma.user.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.user.delete({ where: { id } });
+  }
 } 

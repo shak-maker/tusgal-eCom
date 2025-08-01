70b28601d59b709d2d4342506aa72db2b5b4387a
sga-mudu <411221402@gms.ndhu.edu.tw>
2025-08-01 15:00:51 +0800

    added buttonn - mudu frontend

--- mudu/internship/tusgal/tusgal-brand/src/components/Header.tsx
+++ mudu/internship/tusgal/tusgal-brand/src/components/Header.tsx
@@ -3,7 +3,7 @@ function Header(){
     return(<>
         <div className="sticky w-full h-[80px] md:h-[100px] lg:h-[100px] flex flex-row items-center justify-between bg-[rgba(255,255,255,1)] p-4">
             <a href="" className="w-[100px] md:w-[120px] lg:w-[120px] h-[30px] md:h-[40px] lg:h-[40px] flex items-center md:ml-5">
-                <img src="./brandLogo.png" />
+                <img src="./brandLogo.png" className="ml-0 sm:ml-10 lg:ml-20"/>
             </a>
             <nav className="w-[350px] md:w-[493px] lg:w-[493px] h-[30px] md:h-[25px] lg:h-[25px] ml-auto mr-10 sm:mr-5">
                 <ul className="flex space-x-4 md:space-x-6 lg:space-x-6 justify-center items-center h-full">
